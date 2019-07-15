af76b93ed06e7995a215cf9ef1fa12e4b1b5f387
evanandrewson <evanandrewson@gmail.com>
2019-07-15 16:12:32 -0700

    set up files

---
+++ test/tests.js
@@ -0,0 +1,14 @@
+const test = QUnit.test;
+
+test('name of test', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+   
+
+    //Assert
+    assert.equal(result, expected);
+});
