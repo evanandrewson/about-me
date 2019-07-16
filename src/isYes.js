f56955ac56f6af865c6ac963655efd144d151b00
evanandrewson <evanandrewson@gmail.com>
2019-07-15 17:57:16 -0700

    started creating test for function, end of day 1

---
+++ src/isYes.js
@@ -0,0 +1,5 @@
+function isYes(text) {
+
+}
+
+export default isYes;
--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,15 @@
+import isYes from '../src/isYes.js'
 const test = QUnit.test;
 
 test('name of test', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    
+    const text = "yes";
+    const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
-   
+    const result = isYes(text);
 
     //Assert
     assert.equal(result, expected);
