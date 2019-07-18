09f2594bbc966c9bbb98f2216622d8ebee2b7b7a
evanandrewson <evanandrewson@gmail.com>
2019-07-17 17:28:41 -0700

    finished test for getThrowFromNumber

--- src/getThrow.js
+++ src/getThrow.js
@@ -1,3 +1,9 @@
 export function getThrowFromNumber(number) {
-    
+    if(number === 0) {
+        return 'rock';
+    } else if(number === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
