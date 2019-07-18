8fcd95e0af0c39d1e36074366423f928bd2755de
evanandrewson <evanandrewson@gmail.com>
2019-07-18 10:10:25 -0700

    added betting functionality

--- src/rps.js
+++ src/rps.js
@@ -5,9 +5,12 @@ const resultMessage = document.getElementById('message');
 const winCountDisplay = document.getElementById('win-count-display');
 const lossCountDisplay = document.getElementById('loss-count-display');
 const computerChoice = document.getElementById('computer-choice');
+const bet = document.getElementById('bet');
+const moneyRemaining = document.getElementById('money-remaining');
 
-let winCount = 0
-let lossCount = 0
+let winCount = 0;
+let lossCount = 0;
+let money= 10;
 
 function tieResult() {
     resultMessage.classList.remove('hidden');
@@ -18,23 +21,34 @@ function winResult() {
     resultMessage.textContent = 'You win!';
     winCount++;
     winCountDisplay.textContent = 'Wins: ' + winCount;
+    money += Number(bet.value);
+    moneyRemaining.textContent = 'Money remaining: $' + money;
 }
 function lossResult() {
     resultMessage.classList.remove('hidden');
     resultMessage.textContent = 'You lose!';
     lossCount++;
     lossCountDisplay.textContent = 'Losses: ' + lossCount;
+    money -= Number(bet.value);
+    moneyRemaining.textContent = 'Money remaining: $' + money;
 }
 
 throwButton.addEventListener('click', function() {
     const computerThrow = getThrow();
     const userChoice = document.querySelector('input:checked');
     const userThrow = userChoice.value;
+
+    //Max bet
+    if(bet.value > money) {
+        alert('You can\'t bet more than you have!');
+        return
+    }
+
     // Display computer throw
     computerChoice.classList.remove('invisible');
     const src = 'assets/' + computerThrow + '.jpg';
     computerChoice.src = src;
-
+    
     if(userThrow === computerThrow) {
         tieResult();
     } else if(userThrow === 'rock' && computerThrow === 'scissors') {
@@ -49,5 +63,11 @@ throwButton.addEventListener('click', function() {
         winResult();
     } else if(userThrow === 'scissors' && computerThrow === 'rock') {
         lossResult();
+    }
+
+    // Disable game if out of money
+    if(money === 0) {
+        throwButton.disabled = true;
+        resultMessage.textContent = 'You lose and you\'re out of money! Refresh to play again.'
     }
 });
