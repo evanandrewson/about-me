b4318db3fdb4ffa6bab87a2f41c4e05a9f05a139
evanandrewson <evanandrewson@gmail.com>
2019-07-18 09:30:00 -0700

    completed functionality for game in rps.js, some minor changes to rps.html for UI display changes

--- src/rps.js
+++ src/rps.js
@@ -1,3 +1,54 @@
 import getThrow from './getThrow.js';
-const test = getThrow();
-console.log(test);
+
+const throwButton = document.getElementById('throw-button');
+const resultMessage = document.getElementById('message');
+const winCountDisplay = document.getElementById('win-count-display');
+const lossCountDisplay = document.getElementById('loss-count-display');
+const computerChoice = document.getElementById('computer-choice');
+
+let winCount = 0
+let lossCount = 0
+
+function tieResult() {
+    resultMessage.classList.remove('hidden');
+    resultMessage.textContent = 'Tie game! Try again!';
+}
+function winResult() {
+    resultMessage.classList.remove('hidden');
+    resultMessage.textContent = 'You win!';
+    winCount++;
+    winCountDisplay.textContent = 'Wins: ' + winCount;
+}
+function lossResult() {
+    resultMessage.classList.remove('hidden');
+    resultMessage.textContent = 'You lose!';
+    lossCount++;
+    lossCountDisplay.textContent = 'Losses: ' + lossCount;
+}
+
+throwButton.addEventListener('click', function() {
+    const computerThrow = getThrow();
+    const userChoice = document.querySelector('input:checked');
+    const userThrow = userChoice.value;
+    // Display computer throw
+    computerChoice.classList.remove('invisible');
+    const src = 'assets/' + computerThrow + '.jpg';
+    computerChoice.src = src;
+
+    console.log(userThrow, computerThrow);
+    if(userThrow === computerThrow) {
+        tieResult();
+    } else if(userThrow === 'rock' && computerThrow === 'scissors') {
+        winResult();
+    } else if(userThrow === 'rock' && computerThrow === 'paper') {
+        lossResult();
+    } else if(userThrow === 'paper' && computerThrow === 'rock') {
+        winResult();
+    } else if(userThrow === 'paper' && computerThrow === 'scissors') {
+        lossResult();
+    } else if(userThrow === 'scissors' && computerThrow === 'paper') {
+        winResult();
+    } else if(userThrow === 'scissors' && computerThrow === 'rock') {
+        lossResult();
+    }
+});
