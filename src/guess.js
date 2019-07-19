ded653766b5e365deb53ad87ed8031edc17ca534
Angela Vawser <amvawser@gmail.com>
2019-07-19 09:45:00 -0700

    made guess.js DRY and edited guess.html

--- src/guess.js
+++ src/guess.js
@@ -2,6 +2,7 @@ import compareNumbers from './compareNumbers.js';
 
 const numberInput = document.getElementById('number-input');
 const guessButton = document.getElementById('guess-button');
+const resultMessage = document.getElementById('result-message');
 const highMessage = document.getElementById('high-message');
 const lowMessage = document.getElementById('low-message');
 const winMessage = document.getElementById('win-message');
@@ -12,6 +13,11 @@ const actual = Math.floor(Math.random() * 20 + 1);
 
 let triesLeft = 5;
 
+function changeResultMessage(string) {
+    resultMessage.textContent = string;
+    resultMessage.classList.remove('hidden');
+}
+
 //UI work functions
 //Decrementing tries
 function decrementTries() {
@@ -20,13 +26,11 @@ function decrementTries() {
 }
 //Display of too low message
 function displayLow() {
-    highMessage.classList.add('hidden');
-    lowMessage.classList.remove('hidden');
+    changeResultMessage('Too low!');
 }
 //Display of too high message
 function displayHigh() {
-    highMessage.classList.remove('hidden');
-    lowMessage.classList.add('hidden');
+    changeResultMessage('Too high!');
 }
 //Disable game play
 function disableButton() {
@@ -34,15 +38,11 @@ function disableButton() {
 }
 //Display of lose game
 function displayLose() {
-    highMessage.classList.add('hidden');
-    lowMessage.classList.add('hidden');
-    loseMessage.classList.remove('hidden');
+    changeResultMessage('You lose. Refresh the page to play again with a new number.');
 }
 //Display of win game
 function displayWin() {
-    winMessage.classList.remove('hidden');
-    highMessage.classList.add('hidden');
-    lowMessage.classList.add('hidden');
+    changeResultMessage('You win! Refresh the page to play again with a new number.');
 }
 
 guessButton.addEventListener('click', () => {
